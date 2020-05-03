25eb0ce32a3f32e2bd35a30637e95ea466a31980
Arash Janzadeh <arash@zenops.co.uk>
2020-05-03 01:03:09 +0100

    add functions and comments to passwd

--- passwd.py
+++ passwd.py
@@ -1,28 +1,29 @@
+#! /bin/env python
 # generates a dict of users and how many groups they have
-import time
-import random
-import glob
-import os
-import shutil
-import re
 
-users_file = open('/etc/passwd', "r")
-group_file = open('/etc/group', "r")
+#Cracking the Coding Interview: 189 Programming Questions and Solutions
+
+passwd_file = "/etc/passwd"
+group_file = "/etc/group"
+
+def readfile(filename):
+    with open(filename,'r') as f:
+        data = f.readlines()
+    return(data)
 
 users_membership = {}
 groups = []
 
+for line in readfile(passwd_file):
+    users_membership[line.split(":")[0]] = 0 #O(n)  #ex: root:x:0:0:root:/root:/bin/bash
 
-for line in users_file:
-    users_membership[line.split(":")[0]] = 0
-
-for line in group_file:
+for line in readfile(group_file):
     if line.split(":")[3] != "\n":
-      groups.append(line.split(":")[3].strip("\n").split(","))
+      groups.append(line.split(":")[3].strip("\n").split(",")) #O(n)  # example adm:x:4:aj,arash
 
-for user in users_membership:
+for user in users_membership: # O(n^2)
     for memberes in groups:
-        for member in memberes:
+        for member in memberes: # example if you have few member adm:x:4:aj,arash
           if user == member:
              users_membership[user] += 1
 print(users_membership)
