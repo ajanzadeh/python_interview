6b3fe598ec6f19f1b1f4fc5f1ab110681246fdc7
Arash Janzadeh <arash@zenops.co.uk>
2020-05-06 23:58:58 +0100

    binary tree

---
+++ algorithms/binarytree.py
@@ -0,0 +1,37 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+
+
+    def insert(self,data):
+
+        if self.data:
+            if self.data < data:
+              if self.right == None:
+                 self.right = Node(data)
+              else:
+                  self.right.insert(data)
+            elif self.data > data:
+              if self.left == None:
+                  self.left = Node(data)
+              else:
+                  self.left.insert(data)
+
+
+        else:
+            self.data = data
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print( self.data),
+        if self.right:
+            self.right.PrintTree()
+
+root = Node(10)
+root.insert(29)
+root.insert(18)
+root.insert(1)
+root.PrintTree()
